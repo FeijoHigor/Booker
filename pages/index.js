80d787b04c489a1a1816a039b7113e723165febf
feijohigor <feijohigor@gmail.com>
2023-01-06 16:39:51 -0300

    correcting fetch bug

--- pages/index.js
+++ pages/index.js
@@ -2,8 +2,7 @@ import Display from "../components/_ui/display/index"
 
 export async function getStaticProps(context) {
     const actUrl = process.env.VERCEL_URL
-    const teste = `${process.env.VERCEL_ENV == 'development' ? '' : 'https://'}${actUrl}/api/recomendation`
-    /* const recomendedBooksCall = await fetch(`${process.env.VERCEL_ENV == 'development' ? '' : 'https://'}${actUrl}/api/recomendation`)
+    const recomendedBooksCall = await fetch(`${process.env.VERCEL_ENV == 'development' ? '' : 'https://'}${actUrl}/api/recomendation`)
 
     const recomendedBooksJson = await recomendedBooksCall.json()
     const recomendedBooks = recomendedBooksJson
@@ -12,24 +11,16 @@ export async function getStaticProps(context) {
         props: {
             recomendedBooks
         }
-    } */
-
-    return {
-        props: {
-            teste
-        }
     }
 }
 
 function Home(props) {
 
-    console.log(props.teste)
-
-    //const recomendedBooks = props.recomendedBooks
+    const recomendedBooks = props.recomendedBooks
     return (
         <Display>
             <h1>Hello</h1>
-            {/* <div>
+            <div>
                 {
                     recomendedBooks.mostLiked.map((e, i) => 
                         (
@@ -39,7 +30,7 @@ function Home(props) {
                         )
                     )
                 }
-            </div> */}
+            </div>
         </Display>
     )
 }
