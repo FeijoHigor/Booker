a57c1d88ad0c131ce2bb49191ce3e027b4cd054b
feijohigor <feijohigor@gmail.com>
2023-01-06 15:33:37 -0300

    correct fetch bug

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,8 @@
 import Display from "../components/_ui/display/index"
 
 export async function getStaticProps(context) {
-    const actUrl = process.env.NEXT_PUBLIC_VERCEL_URL
-    const recomendedBooksCall = await fetch(`${actUrl}/api/recomendation`)
+    const actUrl = process.env.VERCEL_URL
+    const recomendedBooksCall = await fetch(`${actUrl}api/recomendation`)
 
     const recomendedBooksJson = await recomendedBooksCall.json()
     const recomendedBooks = recomendedBooksJson
